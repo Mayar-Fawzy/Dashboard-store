9794388a58e7cb6efcff120213d90ae47300dfbc
Mayar-Fawzy <momayar920@gmail.com>
2024-10-13 04:54:11 +0200

    intial project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/core/login/login.component';
+import { NotfoundComponent } from './components/core/notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
